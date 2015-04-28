360a5d0c0b1226a064d3609ae2b73ef183eb14e4
pablobizien <pablobizien>
2015-04-28 12:35:11 +0200

    MultiSelection Spinner

--- src/com/example/cocktail/MainActivity.java
+++ src/com/example/cocktail/MainActivity.java
@@ -4,12 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+  
+public class MainActivity extends Activity {  
+  
+ MultiSelectionSpinner spinner;  
+  
+ @Override  
+ protected void onCreate(Bundle savedInstanceState) {  
+  super.onCreate(savedInstanceState);  
+  setContentView(R.layout.activity_main);  
+  String[] array = { "one", "two", "three" };  
+  spinner = (MultiSelectionSpinner) findViewById(R.id.mySpinner1);  
+  spinner.setItems(array);  
+ }  
+  
+  public void onClick(View v){  
+   String s = spinner.getSelectedItemsAsString();  
+   Toast.makeText(getApplicationContext(), s , Toast.LENGTH_LONG).show();  
+  }  
+}  
