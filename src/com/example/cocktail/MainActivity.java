e650dfc526d2523ffdb75c488c27a0879e9e0ebc
id0052 <id0052@VM-7-HOR_64-5.DOMENSAI.ECOLE>
2015-05-13 19:30:33 +0200

    Rajout de quelques éléments de la fenêtre principale.

--- src/com/example/cocktail/MainActivity.java
+++ src/com/example/cocktail/MainActivity.java
@@ -1,28 +1,33 @@
 package com.example.cocktail;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
   
 public class MainActivity extends Activity {  
   
- MultiSelectionSpinner spinner;  
-  
+ MultiSelectionSpinner spinner1;  
+ MultiSelectionSpinner spinner2;
+ 
+ 
  @Override  
  protected void onCreate(Bundle savedInstanceState) {  
   super.onCreate(savedInstanceState);  
   setContentView(R.layout.activity_main);  
-  String[] array = { "one", "two", "three" };  
-  spinner = (MultiSelectionSpinner) findViewById(R.id.mySpinner1);  
-  spinner.setItems(array);  
+  String[] soft = { "Jus d'orange", "Sirop de grenadine", "Lait" };  
+  String[] alcool = { "Vodka", "Rhum", "Liqueur de Café", "Vin blanc" }; 
+  spinner1 = (MultiSelectionSpinner) findViewById(R.id.mySpinner1);  
+  spinner2 = (MultiSelectionSpinner) findViewById(R.id.mySpinner2);  
+  spinner1.setItems(soft); 
+  spinner2.setItems(alcool);  
+
  }  
   
   public void onClick(View v){  
-   String s = spinner.getSelectedItemsAsString();  
+   String s = spinner1.getSelectedItemsAsString() + ", " + spinner2.getSelectedItemsAsString();  
    Toast.makeText(getApplicationContext(), s , Toast.LENGTH_LONG).show();  
   }  
 }  
